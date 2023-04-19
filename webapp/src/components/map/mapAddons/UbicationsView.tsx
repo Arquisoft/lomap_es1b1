5ada3c5d88d104f6d8296bb5cac22898f3ff9ba4
Jhergig <jhergig@gmail.com>
2023-04-19 14:20:28 +0200

    mapas estaticos en mis ubicaciones

--- webapp/src/components/map/mapAddons/UbicationsView.tsx
+++ webapp/src/components/map/mapAddons/UbicationsView.tsx
@@ -10,17 +10,34 @@ const UbicationsView = () => {
 
     const myUbications = markers.filter((marker) => marker.webId === session.info.webId);
 
+    const cargarMapaEstatico = (lat: number, lng: number) => {
+        const apiKey = "&key=" + process.env.REACT_APP_API_KEY_GOOGLE_MAPS;
+        const URL = "https://maps.googleapis.com/maps/api/staticmap?";
+
+        const centro = "center=" + lat + "," + lng;
+        const zoom ="&zoom=15";
+        const tamaño= "&size=500x500";
+
+        const marcador = "&markers=color:red%7C" + lat + "," + lng;
+        const sensor = "&sensor=false"; 
+
+        const imagenMapa = URL + centro + zoom + tamaño + marcador + sensor + apiKey;
+        return imagenMapa
+    }
+
     return (
         <>
             {myUbications.length > 0 ? (
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ padding: '2em' }}>
                     {myUbications.map((ubication: IPMarker) => (
                         <Grid item xs={6} sm={4} md={3} key={ubication.id}>
-                            <Box sx={{ padding: '1em', bgcolor: 'white', border: 'solid', borderRadius: '2em' }}>
+                            <Box sx={{ padding: '1em', bgcolor: 'white', borderRadius: '0.5em' }}>
                                 <h1 style={{ marginTop: '0em' }}>{ubication.name}</h1>
+                                <img src={cargarMapaEstatico(ubication.lat, ubication.lng)} width='100%' alt="imagen-mapa"/>
                                 <p style={{ marginTop: '0em' }}>Dirección: {ubication.address}</p>
                                 <p>Categoría: {ubication.category}</p>
                                 <p>Descripción: {ubication.description}</p>
+                                <p>Visibilidad: {ubication.id.includes("-") ? (ubication.isPublic ? 'Amigos' : 'Privada') : 'Pública'}</p>
                                 <Button onClick={() => dispatch({ type: Types.DELETE_MARKER, payload: { id: ubication.id } })}>Borrar</Button>
                             </Box>
                         </Grid>
