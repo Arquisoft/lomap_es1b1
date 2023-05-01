ee67abe26c2b9b283f9bdf1f49175130923026b9
Jhergig <jhergig@gmail.com>
2023-05-01 21:30:13 +0200

    homepage

--- webapp/src/components/HomeView.tsx
+++ webapp/src/components/HomeView.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@mui/material";
+import { Button, Grid, Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { FOAF } from "@inrupt/vocab-common-rdf";
 import { CombinedDataProvider, Text, useSession } from "@inrupt/solid-ui-react";
@@ -8,7 +9,7 @@ const HomeView = () => {
   const { session: { info: { isLoggedIn, webId } } } = useSession();
 
   return (
-    <Box sx={{ color: 'white', textAlign: 'center' }}>
+    <Stack spacing={'2em'} sx={{ color: 'white', alignItems: 'center' }}>
       <h1>
         {t("HomeView.welcome")}
         {isLoggedIn && (
@@ -19,8 +20,27 @@ const HomeView = () => {
         )}
         !
       </h1>
-    </Box>
+      <Grid container alignItems="center" spacing='2em' sx={{
+        width: {
+          xs: '80%',
+          sm: '80%',
+          md: '55em',
+        }, height: '100%'
+      }}>
+        <Grid item xs={12} md={3}>
+          <img src="/solid.svg" className="App-logo" alt="LoMap" width={'100%'} />
+        </Grid>
+        <Grid item xs={12} md={9}>
+          <p align='justify'>{t("HomeView.p1")}</p>
+          <p align='justify'>{t("HomeView.p2")}</p>
+          <p align='justify'>{t("HomeView.p3")}</p>
+        </Grid>
+      </Grid>
+      <Link to="/map">
+        <Button className='greenButton' variant="contained" sx={{ my: 2 }}>{t("HomeView.start")}</Button>
+      </Link>
+    </Stack>
   );
 }
 
-export default HomeView;
+export default HomeView; 
