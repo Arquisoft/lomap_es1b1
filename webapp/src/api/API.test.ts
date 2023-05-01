55453ad491cc19f1ab6e113e8678ba93f2ad2aff
Jhergig <jhergig@gmail.com>
2023-05-01 07:42:45 +0200

    api tests

--- webapp/src/api/API.test.ts
+++ webapp/src/api/API.test.ts
@@ -2,37 +2,33 @@ import { addPublicLocation, deletePublicLocation, getPublicLocations, updatePubl
 
 
 test("funcion para listar direcciones", async () => {
-        try {
         let dir = await getPublicLocations()
-        } catch (error) {}
+        expect(dir.length > 0)
 });
 
 test("funcion para añadir direccion", async () => {
-        try {
         let dir = await addPublicLocation({
             id: "test", date: new Date(), lat: 0, lng: 0, name: "Sin nombre", address: "Sin dirección",
             category: "Sin categoría", isPublic: true, description: "Sin descripción", canFriendsSee:false,
             reviews: [], webId: ""
         })
-        } catch (error) {}
+        expect(dir === true)
 });
 
 test("funcion para actualizar direcciones", async () => {
-    try {
     let dir = await updatePublicLocation({
         id: "test", date: new Date(), lat: 0, lng: 0, name: "Sin nombre", address: "Sin dirección",
         category: "Sin categoría", isPublic: true, description: "Sin descripción", canFriendsSee:false,
         reviews: [], webId: ""
     })
-    } catch (error) {}
+    expect(dir === true)
 });
 
 test("funcion para borrar direcciones", async () => {
-    try {
     let dir = await deletePublicLocation({
         id: "test", date: new Date(), lat: 0, lng: 0, name: "Sin nombre", address: "Sin dirección",
         category: "Sin categoría", isPublic: true, description: "Sin descripción", canFriendsSee:false,
         reviews: [], webId: ""
     })
-    } catch (error) {}
+    expect(dir === true)
 });
