055360685bb0555ac2159a38429028b0419af10e
Jhergig <jhergig@gmail.com>
2023-05-01 21:58:27 +0200

    homepage tests

--- webapp/src/components/HomeView.test.tsx
+++ webapp/src/components/HomeView.test.tsx
@@ -1,8 +1,9 @@
 import { render } from "@testing-library/react";
 import HomeView from "./HomeView";
+import { BrowserRouter } from "react-router-dom";
 
 test('check that HomeView not logged in is rendering propertly', async () => {
-    const { getByText } = render(<HomeView></HomeView>);
+    const { getByText } = render(<BrowserRouter><HomeView/></BrowserRouter>);
 
     expect(getByText("HomeView.welcome!")).toBeInTheDocument();
 });
